3a49a36d523432497380bc709951c814436d3218
JaewonKil <kiljw316@gmail.com>
2026-08-06 00:52:34 +0900

    formatting

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -1,12 +1,12 @@
 package auth
 
 import (
-    "reflect"
-    "testing"
+	"reflect"
+	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
-    if !reflect.DeepEqual(1, 1) {
-        t.Fatalf("test fail")
-    }
+	if !reflect.DeepEqual(1, 1) {
+		t.Fatalf("test fail")
+	}
 }
